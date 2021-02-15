16a305e164ebac288c691657abe6b0832d3959fd
Lars Toft Jacobsen <1213126+ltj@users.noreply.github.com>
2021-02-15 17:15:20 +0100

    fix: changed invalid gh packages url

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            url = uri("https://maven.pkg.github.com/imotions/bson4k")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
