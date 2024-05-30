bfb28c47f63d564fff55cb53c95166dd57029102
Lars Jacobsen <1213126+ltj@users.noreply.github.com>
2024-05-30 09:26:51 +0200

    build: refresh gh workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,8 +50,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/ltj/bson4k")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
             }
         }
     }
